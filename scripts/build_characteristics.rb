02e4474562027c787f1bb75da92a5ebf7d672f97
Lucas Sotty <lsotty@mesvaccins.net>
2026-05-25 16:17:35 +0200

    Translations

--- scripts/build_characteristics.rb
+++ scripts/build_characteristics.rb
@@ -3,9 +3,11 @@ require "yaml"
 require "fileutils"
 require "medcon"
 
-BASE_PATH = "characteristics".freeze
+CHARACTERISTICS_BASE_PATH = "characteristics".freeze
+TRANSLATIONS_BASE_PATH    = "translations".freeze
+ADDITIONAL_LANGUAGES      = %w[fr].freeze
 
-def handle_groups(groups, groups_by_parent_id, conditions_by_id, parents)
+def handle_groups(groups, groups_by_parent_id, conditions_by_id, additional_medcons, parents)
   groups.each do |group|
     group.condition_ids.each do |condition_id|
       condition = conditions_by_id[condition_id]
@@ -13,27 +15,44 @@ def handle_groups(groups, groups_by_parent_id, conditions_by_id, parents)
       condition_data = {
         "id" => condition.code.to_i,
         "label" => condition.label,
+        "description" => condition.help,
         "type" => condition.type.to_s.downcase,
         "tags" => [*parents, group.label],
         "codes" => [{ "nomenclature" => "SYADEM", "code" => condition.id }]
       }
 
-      File.write("#{BASE_PATH}/C-#{condition.code}.yml", condition_data.to_yaml(line_width: -1))
+      File.write("#{CHARACTERISTICS_BASE_PATH}/C-#{condition.code}.yml", condition_data.to_yaml(line_width: -1))
+
+      additional_medcons.each do |lang, medcon|
+        translated_condition = medcon.repositories.conditions.find(condition_id)
+
+        translation_data = {
+          "label" => translated_condition.label,
+          "description" => translated_condition.help
+        }
+
+        File.write("#{TRANSLATIONS_BASE_PATH}/#{lang}/C-#{condition.code}.yml", translation_data.to_yaml(line_width: -1))
+      end
     end
 
     children = groups_by_parent_id[group.id]
 
-    handle_groups(children, groups_by_parent_id, conditions_by_id, [*parents, group.label]) if children
+    next if children.nil?
+
+    handle_groups(children, groups_by_parent_id, conditions_by_id, additional_medcons, [*parents, group.label])
   end
 end
 
-medcon = Medcon::Medcon.load(lang: "en")
+medcon             = Medcon::Medcon.load(lang: "en")
+additional_medcons = ADDITIONAL_LANGUAGES.each_with_object({}) { |lang, obj| obj[lang] = Medcon::Medcon.load(lang:) }
 
-FileUtils.rm_rf(BASE_PATH)
-FileUtils.mkdir_p(BASE_PATH)
+FileUtils.rm_rf(CHARACTERISTICS_BASE_PATH)
+FileUtils.rm_rf(TRANSLATIONS_BASE_PATH)
+FileUtils.mkdir_p(CHARACTERISTICS_BASE_PATH)
+ADDITIONAL_LANGUAGES.each { |lang| FileUtils.mkdir_p("#{TRANSLATIONS_BASE_PATH}/#{lang}") }
 
 conditions_by_id    = medcon.repositories.conditions.all.map { |c| [c.id, c] }.to_h
 groups_by_parent_id = medcon.repositories.condition_groups.all.group_by(&:parent_id)
 root_groups         = groups_by_parent_id[""]
 
-handle_groups(root_groups, groups_by_parent_id, conditions_by_id, [])
+handle_groups(root_groups, groups_by_parent_id, conditions_by_id, additional_medcons, [])
